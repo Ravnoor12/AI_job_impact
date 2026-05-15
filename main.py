2f9723360fa3e79d2bfbbf2e205fb01e4c8ed04e
Ravnoor Singh <ravnoor12@gmail.com>
2026-05-14 23:11:11 -0400

    Data validation pipeline completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from jobImpact.components.data_ingestion import DataIngestion
-from jobImpact.entity.config_entity import DataIngestionConfig
+from jobImpact.components.data_validation import DataValidation
+from jobImpact.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
+from jobImpact.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from jobImpact.entity.config_entity import TrainingPipelineConfig
 from jobImpact.logging.logger import logging
 from jobImpact.exception.exception import JobImpactException
@@ -14,6 +16,14 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(data_ingestion_config)
         logging.info("initiating data ingestion")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion completed and artifact is created")
+        
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation( data_ingestion_artifact, data_validation_config)
+        logging.info("initiating data validation")
+        data_validation.initiate_data_validation()
+        logging.info("Data validation completed and artifact is created")
         print(data_ingestion_artifact)
+
     except Exception as e:
         raise JobImpactException(e,sys)
